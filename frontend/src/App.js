94650e4375df30b3ea8f72821ee0b30e3552b212
Trina Dasgupta <88958773+Trina-Dasgupta@users.noreply.github.com>
2022-10-30 20:10:06 +0530

    Add files via upload

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+//import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+function App() {
+  return (
+    <div className="App">
+     Hello Everyone...
+    </div>
+  );
+}
+
+export default App;
